a43066edfbc00d5cc84406129db1b86117917f3c
boakaidkamara <boakaidkamara@gmail.com>
2023-10-08 18:08:24 +0000

    installed material UI and created header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Header from "./component/header/header";
+
 function App() {
-  return <p className="bg-sky-500">Working</p>;
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header />
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,11 +1,19 @@
+import React from "react";
+// import Grid from "@mui/material/Grid";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import Box from "@mui/material/Box";
+
 function Header() {
   return (
-    <div>
+    <Box display="flex" alignItems="center" justifyContent="space-between">
       <h1 className="text-3xl font-bold underline">dev-explorer</h1>
       <div>
-        <p>Light</p>
+        <Box display="flex" alignItems="center">
+          <p>LIGHT</p>
+          <LightModeIcon />
+        </Box>
       </div>
-    </div>
+    </Box>
   );
 }
 
